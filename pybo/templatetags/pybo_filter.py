83a33371af466cd48699c52ecf0b6ad90698a5ae
songyouhyun <doong3373@gmail.com>
2021-02-16 18:20:40 +0900

    검색, 정렬 기능 추가하기

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -5,12 +5,12 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
-@register.filter()
+@register.filter
 def sub(value, arg):
     return value - arg
 
 
 @register.filter()
 def mark(value):
-    extensions = ['nl2br', "fenced_code"]
+    extensions = ["nl2br", "fenced_code"]
     return mark_safe(markdown.markdown(value, extensions=extensions))
--- pybo/views/comment_views.py
+++ pybo/views/comment_views.py
@@ -80,7 +80,7 @@ def comment_create_answer(request, answer_id):
             comment.create_date = timezone.now()
             comment.answer = answer
             comment.save()
-            return redirect('{}#comment_{}'.format(resolve_url('pybo:detail', question_id=comment.answer.id), comment.id))
+            return redirect('{}#comment_{}'.format(resolve_url('pybo:detail', question_id=comment.answer.question.id), comment.id))
     else:
         form = CommentForm()
     context = {'form': form}
@@ -104,7 +104,7 @@ def comment_modify_answer(request, comment_id):
             comment.author = request.user
             comment.modify_date = timezone.now()
             comment.save()
-            return redirect('{}#comment_{}'.format(resolve_url('pybo:detail', question_id=comment.answer.id), comment.id))
+            return redirect('{}#comment_{}'.format(resolve_url('pybo:detail', question_id=comment.answer.question.id), comment.id))
     else:
         form = CommentForm(instance=comment)
     context = {'form': form}
